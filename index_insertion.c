a1de1af4a74299abddcd8afef052050be1bfbbc8
Rahulv1904 <118659527+Rahulv1904@users.noreply.github.com>
2022-11-20 15:08:21 +0530

    Update index_insertion.c

--- index_insertion.c
+++ index_insertion.c
@@ -2,37 +2,38 @@
 #include<stdio.h>
 int in(){
     int n;
-scanf("%d",&n);
-return n;
+    scanf("%d",&n);
+    return n;
 }
 int input(int a[], int n){
-for(int i=0;i<n;i++)
- scanf("%d",&a[i]);
-return 1;
+    for(int i=0;i<n;i++)
+       scanf("%d",&a[i]);
+    return 1;
 };
 
+//Logic
 int brain(int a[],int n,int r,int z){
     for(int i=n;i>z;i--)
-   a[i]=a[i-1];
-a[z]=r;
-return 1;
+       a[i]=a[i-1];
+    a[z]=r;
+    return 1;
 };
 
 int output(int a[75],int n){
     for(int i=0;i<n+1;i++)
-printf("%d ",a[i]);
-return 0;
+       printf("%d ",a[i]);
+    return 0;
 }
 int main(){
-int i,a[75],r,n,z;
-n=in();
-input(a,n);
-printf("Enter the index of element to be inserted\n");
-scanf("%d",&z);
-z=z-1;
-printf("enter the elements to be inserted\n");
-scanf("%d",&r);
-brain(a,n,r,z);
-output(a,n);
-return 0;
+ int i,a[75],r,n,z;
+ n=in();
+ input(a,n);
+ printf("Enter the index of element to be inserted\n");
+ scanf("%d",&z);
+ z=z-1;
+ printf("enter the elements to be inserted\n");
+ scanf("%d",&r);
+ brain(a,n,r,z);
+ output(a,n);
+ return 0;
 }
